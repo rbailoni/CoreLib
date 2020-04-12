8b062cf7bb8e12f009e6ca6079ad9a2844017fc1
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2020-04-12 01:50:37 -0300

    Initial project setup

---
+++ CoreLibSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreLibSample
+//
+//  Created by Ricardo Bailoni on 12/04/20.
+//  Copyright © 2020 Ricardo Bailoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
