dcf8c5a777dd8ac405b377870cc1227cd05ed563
Ricardo Bailoni <rbailoni@rbailoni.com.br>
2020-04-12 01:41:31 -0300

    Initial Commit

---
+++ CoreLib/CoreLib.h
@@ -0,0 +1,19 @@
+//
+//  CoreLib.h
+//  CoreLib
+//
+//  Created by Ricardo Bailoni on 12/04/20.
+//  Copyright © 2020 Ricardo Bailoni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreLib.
+FOUNDATION_EXPORT double CoreLibVersionNumber;
+
+//! Project version string for CoreLib.
+FOUNDATION_EXPORT const unsigned char CoreLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreLib/PublicHeader.h>
+
+
